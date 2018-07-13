c55eb62a1c4e5d122c3155189cbf310b523fb92f
Robinson Marquez <nosnibor1989@gmail.com>
2018-07-13 12:14:05 -0400

    Initial Commit

---
+++ iTodo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iTodo
+//
+//  Created by Robinson Marquez on 7/13/18.
+//  Copyright © 2018 Robinson Marquez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
